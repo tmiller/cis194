b8a450ca24dfba0d8299ac0962a27df11dee37d8
Tom Miller <jackerran@gmail.com>
2014-07-25 15:17:13 -0400

    Solve excercises 1 - 4
    Build up a credit card validator

---
+++ hw1/CreditCard.hs
@@ -0,0 +1,23 @@
+-- CIS 194: Homework 1
+-- Author: Tom Miller
+
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toDigitsRev
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x | x <= 0 = []
+              | x > 0 = let digit = x `rem` 10
+                            rest  = truncate $ (toRational x) / 10
+                        in  digit : toDigitsRev rest
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = fst . foldr doubleEveryOther' ([], False) 
+    where doubleEveryOther' x (xs, double) = if   double
+                                             then (x*2:xs, False)
+                                             else (x:xs, True)
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concat . map toDigits
+
+validate :: Integer -> Bool
+validate = (==0) . (`rem`10) . sumDigits . doubleEveryOther . toDigits
